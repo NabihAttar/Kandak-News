0bdaab2e1dd636c878df166a33f665466ead5c45
Nabih Attar <nabihattarr@gmail.com>
2025-10-14 10:39:03 +0300

    Initial commit

--- src/app/(pages)/article/page.jsx
+++ src/app/(pages)/article/page.jsx
@@ -7,16 +7,16 @@ import AuthorProfileCard from "./AuthorProfileCard";
 
 const ArticlePage = () => {
   return (
-    <div className="col-12 post-content pb-5 border-l-2 border-gray-300 bg-white">
+    <div className="col-12 post-content pb-5 bg-white border-l-0 lg:border-l-2 lg:border-gray-300">
       {/* Header with background image */}
       <ArticleHeader />
 
       <div className="container mx-auto mt-12 px-4">
         <div className="flex flex-col lg:flex-row items-start gap-6">
           {/* Main content area */}
-  <div className="w-2/3 min-w-0 border-l border-gray-600 pl-8">
+          <div className="w-full lg:w-2/3 min-w-0 pl-0 lg:pl-8 lg:border-l lg:border-gray-600">
             <div className="mb-6">
-              <h1 className="text-3xl font-bold mb-4 text-black mr-6" dir="rtl">
+              <h1 className="text-3xl font-bold mb-4 text-black mr-0 lg:mr-6" dir="rtl">
                 مع جيجك: لماذا لا أزال شيوعيّاً
               </h1>
             </div>
@@ -28,14 +28,16 @@ const ArticlePage = () => {
             {/* Article content */}
             <ArticleContent />
 
-            {/* Author Profile Card at the bottom of main content */}
-
+            {/* If you want the author profile at the end */}
+            {/* <div className="mt-10">
+              <AuthorProfileCard />
+            </div> */}
           </div>
 
           {/* Sidebar */}
-          <div className="w-full lg:w-1/3 pl-6">
+          <aside className="w-full lg:w-1/3 pl-0 lg:pl-6">
             <PostsList />
-          </div>
+          </aside>
         </div>
       </div>
     </div>
